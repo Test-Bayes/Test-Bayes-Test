201f988607f12cd0d718dd9e1c7fc818d3ebc3d7
Avidant Bhagat <avidant@cs.washington.edu>
2018-05-30 19:51:36 -0700

    Adds more tests

--- src/test/java/model/ModelATest.java
+++ src/test/java/model/ModelATest.java
@@ -30,4 +30,33 @@ public class ModelATest {
         }
     }
 
+    @Test
+    public void testEqualsAndHashcode() {
+        Random random = new Random();
+        for(int i = 0; i < 10; i++) {
+            ModelA model1 = new ModelA();
+            ModelA model2 = new ModelA();
+            String name = Utils.randomAlphaNumeric();
+            model1.setName(name);
+            model2.setName(name);
+            int value = random.nextInt();
+            model1.setValue(value);
+            model2.setValue(value);
+            assertEquals(model1, model2);
+            assertEquals(model1.hashCode(), model2.hashCode());
+        }
+    }
+
+    @Test
+    public void testToString() {
+        ModelA modelA = new ModelA();
+        String name = Utils.randomAlphaNumeric();
+        modelA.setName(name);
+        Random random = new Random();
+        int value = random.nextInt();
+        modelA.setValue(value);
+        String expected = "ModelA{name=" + name + ", value=" + value + "}";
+        assertEquals(expected, modelA.toString());
+    }
+
 }
