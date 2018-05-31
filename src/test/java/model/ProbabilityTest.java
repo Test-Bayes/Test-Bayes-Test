e58a9f66d9201bb693130f29fd829cf1bdeef051
Avidant Bhagat <avidant@cs.washington.edu>
2018-05-30 22:38:22 -0700

    Adds functionality

--- src/test/java/model/ProbabilityTest.java
+++ src/test/java/model/ProbabilityTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(TestBayesIndividualClassRunner.class)
+@RunWith(JUnitWrapper.class)
 public class ProbabilityTest {
 
     /**
