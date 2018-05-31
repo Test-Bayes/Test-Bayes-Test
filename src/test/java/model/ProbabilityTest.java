6f78007d8d10c02ca936c4ec3a5e76774f8b139a
Aditya Jhamb <aditya97@cs.washington.edu>
2018-05-30 22:20:56 -0700

    probability 3 Random

--- src/test/java/model/ProbabilityTest.java
+++ src/test/java/model/ProbabilityTest.java
@@ -1,6 +1,7 @@
 package model;
 
 import edu.uw.cse.testbayes.model.Probability;
+import edu.uw.cse.testbayes.runner.RandomIndividualClassRunner;
 import edu.uw.cse.testbayes.runner.TestBayesIndividualClassRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,7 +10,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(TestBayesIndividualClassRunner.class)
+@RunWith(RandomIndividualClassRunner.class)
 public class ProbabilityTest {
 
     /**
